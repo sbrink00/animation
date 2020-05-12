f0632e47af3834cca45a1ad735de808ab2d2087c
sbrink00 <sbrink00@stuy.edu>
2020-05-11 22:17:12 -0400

    animation tentatively works but weird stuff was happening with sphere

--- script.py
+++ script.py
@@ -2,9 +2,7 @@ import mdl
 from display import *
 from matrix import *
 from draw import *
-
-knobCommands = ["move", "rotate", "scale", "set"]
-threeDStuffs = ["box", "sphere", "torus"]
+import subprocess
 
 def dc(ary):
   return [x if type(x) is not list else dc(x) for x in ary]
@@ -57,7 +55,6 @@ def second_pass( commands, num_frames ):
     for command in varies:
       knob_name = command["knob"]
       knobs.append(knob_name)
-      print(knob_name)
       start_frame = int(command["args"][0])
       end_frame = int(command["args"][1])
       if end_frame < start_frame:
@@ -89,7 +86,6 @@ def run(filename):
         raise Exception("Unable to parse file.")
     basename, num_frames, animCommands = first_pass(commands)
     knobValues = second_pass(animCommands, num_frames)
-    print(knobValues)
     view = [0,
             0,
             1];
@@ -121,82 +117,96 @@ def run(filename):
                          {'red': [0.2, 0.5, 0.5],
                           'green': [0.2, 0.5, 0.5],
                           'blue': [0.2, 0.5, 0.5]}]
-    for i,command in enumerate(commands):
-        c = command["op"]
-        reflect = ".white"
-        if c in knobCommands: knob = command["knob"]
-        if c in threeDStuffs:
-          const = command["constants"]
-          if const != None: reflect = const
-          if command["cs"] != None: pass
-        args = command["args"]
-        if c == "save":
-          save_extension(screen, args[0] + ".png")
-          print("Filename: " + args[0] + ".png")
-          screen.clear()
-        if c == "line":
-          if command["cs0"] != None: pass
-          if command["cs1"] != None: pass
-          add_edge( edges,
-                    float(args[0]), float(args[1]), float(args[2]),
-                    float(args[3]), float(args[4]), float(args[5]) )
-          matrix_mult(stack[-1], edges)
-          draw_lines(edges, screen, zbuffer, color)
-          edges = []
-        if c == "curve": pass
-        if c == "sphere":
-          add_sphere(polygons,
-                     float(args[0]), float(args[1]), float(args[2]),
-                     float(args[3]), step_3d)
-          matrix_mult(stack[-1], polygons)
-          draw_polygons(polygons, screen, zbuffer, view, ambient, light, symbols, reflect)
-          polygons = []
-        if c == "box":
-          add_box(polygons,
-                     float(args[0]), float(args[1]), float(args[2]),
-                     float(args[3]), float(args[4]), float(args[5]))
-          matrix_mult(stack[-1], polygons)
-          draw_polygons(polygons, screen, zbuffer, view, ambient, light, symbols, reflect)
-          polygons = []
-        if c == "torus":
-          const = command["constants"]
-          add_torus(polygons,
-                     float(args[0]), float(args[1]), float(args[2]),
-                     float(args[3]), float(args[4]), step_3d)
-          matrix_mult(stack[-1], polygons)
-          draw_polygons(polygons, screen, zbuffer, view, ambient, light, symbols, reflect)
-          polygons = []
-        if c == "pop":
-          stack.pop()
-        if c == "push":
-          stack.append(dc(stack[-1]))
-        if c == "move":
-          if knob != None: pass
-          t = make_translate(float(args[0]), float(args[1]), float(args[2]))
-          matrix_mult(stack[-1], t)
-          stack[-1] = dc(t)
-        if c == "rotate":
-          theta = float(args[1]) * (math.pi / 180)
-          if args[0] == 'x':
-              t = make_rotX(theta)
-          elif args[0] == 'y':
-              t = make_rotY(theta)
-          else:
-              t = make_rotZ(theta)
-          matrix_mult(stack[-1], t)
-          stack[-1] = dc(t)
-        if c == "scale":
-          t = make_scale(float(args[0]), float(args[1]), float(args[2]))
-          matrix_mult(stack[-1], t)
-          stack[-1] = dc(t)
-        #it seems like the mdl.py file already does everything I need for constants
-        if c == "constants": pass
-        #to be coded later
-        if c == "save_knobs": pass
-        if c == "tween": pass
-        if c == "light": pass
-        if c == "mesh": pass
-        if c == "basename": pass
+    knobCommands = ["move", "rotate", "scale"]
+    threeDStuffs = ["box", "sphere", "torus"]
+    images = []
+    for x in range(num_frames):
+      for i,command in enumerate(commands):
+          args = command["args"]
+          c = command["op"]
+          reflect = ".white"
+          if c in knobCommands:
+            knob = command["knob"]
+            val = knobValues[x][knob]
+            args = [x * val for x in args]
+            print(args)
+          if c in threeDStuffs:
+            const = command["constants"]
+            if const != None: reflect = const
+            if command["cs"] != None: pass
+          if c == "save":
+            save_extension(screen, args[0] + ".png")
+            print("Filename: " + args[0] + ".png")
+            screen.clear()
+          if c == "line":
+            if command["cs0"] != None: pass
+            if command["cs1"] != None: pass
+            add_edge( edges,
+                      float(args[0]), float(args[1]), float(args[2]),
+                      float(args[3]), float(args[4]), float(args[5]) )
+            matrix_mult(stack[-1], edges)
+            draw_lines(edges, screen, zbuffer, color)
+            edges = []
+          if c == "curve": pass
+          if c == "sphere":
+            add_sphere(polygons,
+                       float(args[0]), float(args[1]), float(args[2]),
+                       float(args[3]), step_3d)
+            matrix_mult(stack[-1], polygons)
+            draw_polygons(polygons, screen, zbuffer, view, ambient, light, symbols, reflect)
+            polygons = []
+          if c == "box":
+            add_box(polygons,
+                       float(args[0]), float(args[1]), float(args[2]),
+                       float(args[3]), float(args[4]), float(args[5]))
+            matrix_mult(stack[-1], polygons)
+            draw_polygons(polygons, screen, zbuffer, view, ambient, light, symbols, reflect)
+            polygons = []
+          if c == "torus":
+            const = command["constants"]
+            add_torus(polygons,
+                       float(args[0]), float(args[1]), float(args[2]),
+                       float(args[3]), float(args[4]), step_3d)
+            matrix_mult(stack[-1], polygons)
+            draw_polygons(polygons, screen, zbuffer, view, ambient, light, symbols, reflect)
+            polygons = []
+          if c == "pop":
+            stack.pop()
+          if c == "push":
+            stack.append(dc(stack[-1]))
+          if c == "move":
+            if knob != None: pass
+            t = make_translate(float(args[0]), float(args[1]), float(args[2]))
+            matrix_mult(stack[-1], t)
+            stack[-1] = dc(t)
+          if c == "rotate":
+            theta = float(args[1]) * (math.pi / 180)
+            if args[0] == 'x':
+                t = make_rotX(theta)
+            elif args[0] == 'y':
+                t = make_rotY(theta)
+            else:
+                t = make_rotZ(theta)
+            matrix_mult(stack[-1], t)
+            stack[-1] = dc(t)
+          if c == "scale":
+            t = make_scale(float(args[0]), float(args[1]), float(args[2]))
+            matrix_mult(stack[-1], t)
+            stack[-1] = dc(t)
+          #it seems like the mdl.py file already does everything I need for constants
+          if c == "constants": pass
+          #to be coded later
+          if c == "save_knobs": pass
+          if c == "tween": pass
+          if c == "light": pass
+          if c == "mesh": pass
+          if c == "basename": pass
+      fname = "anim/" + basename + str(x) + ".png"
+      images.append(fname)
+      save_extension(screen, fname)
+      screen = new_screen()
+    termCommand = ["convert"] + images  + ["movie.gif"]
+    subprocess.run(termCommand)
 
 def pass2():
   pass
